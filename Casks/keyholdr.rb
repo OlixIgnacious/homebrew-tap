36c8607c7eb3c8074f39a5efff98b70de27d368c
Ashwini Sharma <ashwini.sharma0807@gmail.com>
2026-06-12 23:28:16 +0530

    Add verified parameter to url stanza

--- Casks/keyholdr.rb
+++ Casks/keyholdr.rb
@@ -2,7 +2,8 @@ cask "keyholdr" do
   version "1.1.0"
   sha256 "d92d9ca3910481712636fe82f4c70ec9f7de7adec1dd430253908f01898d90a3"
 
-  url "https://github.com/OlixIgnacious/keyholdr/releases/download/v#{version}/Keyholdr-macOS-v#{version}.zip"
+  url "https://github.com/OlixIgnacious/keyholdr/releases/download/v#{version}/Keyholdr-macOS-v#{version}.zip",
+      verified: "github.com/OlixIgnacious/keyholdr/"
   name "Keyholdr"
   desc "Native menu bar vault for API keys with Touch ID unlock"
   homepage "https://olixignacious.github.io/keyholdr/"
